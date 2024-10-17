90b0a9717364607c17e2d6d4d2b14ee6319a1065
Eduardo Melo <eduardoddmg@gmail.com>
2024-10-16 22:53:08 -0300

    up

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 // app/api/auth/[...nextauth]/route.ts
+import 'next-auth/jwt';
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { auth } from '@/firebase';
